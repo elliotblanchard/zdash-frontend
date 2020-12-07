2ad885fb70ec89fec2bedae374fad9f0d87f1786
Elliot Blanchard <git@elliotblanchard.com>
2020-12-07 09:20:36 -0500

    Set up most imports for Redux

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/networks')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
-
   render () {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
