bc891526bf7d8dccf10455fc469f88dccf89fb3a
Elliot Blanchard <git@elliotblanchard.com>
2020-12-22 20:48:12 -0500

    Implemented area bump chart

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -33,8 +33,8 @@ function Bar(props) {
         let axisBottom = {
                 tickSize: 5,
                 tickPadding: 5,
-                tickRotation: 0,
-                legend: 'day',
+                tickRotation: 90,
+                legend: '',
                 legendPosition: 'middle',
                 legendOffset: 32
             } 
--- src/components/Bump.js
+++ src/components/Bump.js
@@ -12,15 +12,40 @@ function Bump(props) {
                 if (!categoryHash[categoryName]) {
                     categoryHash[categoryName] = {id:categoryName,data:[]}
                 } 
-                categoryHash[categoryName].data.push({x:transaction.display_time, y:category[1]})
+                const percentage = category[1] / transaction.total
+                categoryHash[categoryName].data.push({x:transaction.display_time, y:percentage})
             })                
         }) 
-        console.log(categoryHash)        
-
+        data = Object.values(categoryHash)       
+        console.log(data)
 		return ( 
-			<div>
-				Hello
-			</div>
+            <ResponsiveAreaBump
+            data={data}
+            margin={{ top: 50, right: 120, bottom: 75, left: 120 }}
+            spacing={8}            
+            colors={{ scheme: 'nivo' }}
+            blendMode="multiply"  
+            startLabel="id"
+            axisTop={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 90,
+                legend: '',
+                legendPosition: 'middle',
+                legendOffset: -36
+            }}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 90,
+                legend: '',
+                legendPosition: 'middle',
+                legendOffset: 32
+            }}
+             animate={true}
+             motionStiffness={90}
+             motionDamping={15}
+         />                      
 		)               
 	} else {
 		return ( 
