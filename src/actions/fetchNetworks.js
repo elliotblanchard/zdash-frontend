8c833fd9db7302649da47ee1b8ded7a633ab7ff6
Elliot Blanchard <git@elliotblanchard.com>
2021-01-16 13:23:41 -0500

    Switched fetch actions back to Heroku

--- src/actions/fetchNetworks.js
+++ src/actions/fetchNetworks.js
@@ -1,8 +1,8 @@
 export function fetchNetworks(data) {
     return (dispatch) => {
        dispatch({ type: 'LOADING_NETWORKS'})
-       fetch(`http://localhost:3000/api/v1/networks`)
-       //fetch('https://damp-citadel-47471.herokuapp.com/api/v1/networks')
+       fetch('https://damp-citadel-47471.herokuapp.com/api/v1/networks')
+       //fetch(`http://localhost:3000/api/v1/networks`)
        .then(resp => resp.json())
        .then(networks => dispatch({
           type: 'ADD_NETWORKS',
--- src/actions/fetchTransactions.js
+++ src/actions/fetchTransactions.js
@@ -2,8 +2,8 @@ export function fetchTransactions(time) {
    console.log(time)
    return (dispatch) => {
       dispatch({ type: 'LOADING_TRANSACTIONS'})
-      //fetch(`https://damp-citadel-47471.herokuapp.com/api/v1/transactions/${time}`)
-      fetch(`http://localhost:3000/api/v1/transactions/${time}`)
+      fetch(`https://damp-citadel-47471.herokuapp.com/api/v1/transactions/${time}`)
+      //fetch(`http://localhost:3000/api/v1/transactions/${time}`)
       .then(resp => resp.json())
       .then(transactions => dispatch({
          type: 'ADD_TRANSACTIONS',
