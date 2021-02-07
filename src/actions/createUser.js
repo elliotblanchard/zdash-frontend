5ca490525e34c06cc75ee3211deef71d03f8f8f3
Elliot Blanchard <git@elliotblanchard.com>
2021-02-07 17:22:48 -0500

    Commit for review

--- src/actions/createUser.js
+++ src/actions/createUser.js
@@ -3,7 +3,7 @@ export function createUser(name, address) {
         dispatch({ type: 'LOADING_FLAG'})
         const bodyData = { user: { name, address } }
         //fetch(`https://damp-citadel-47471.herokuapp.com/api/v1/users`)
-        fetch(`http://localhost:3000/api/v1/users`, {
+        fetch(`http://localhost:3000/api/v1/users`,{
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(bodyData),
