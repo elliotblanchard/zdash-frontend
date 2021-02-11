795615e6b14016f48e9b5a8cf4e175fccd7c0c4e
Elliot Blanchard <git@elliotblanchard.com>
2021-02-11 08:49:09 -0500

    Buttoned up for release

--- src/actions/fetchAccountDetail.js
+++ src/actions/fetchAccountDetail.js
@@ -2,8 +2,8 @@ export function fetchAccountDetail(data) {
     return (dispatch) => {
        dispatch({ type: 'LOADING_ACCOUNT_DETAIL'})
        dispatch({ type: 'LOADING_FLAG'})        
-       //fetch(`https://damp-citadel-47471.herokuapp.com/api/v1/accounts/${data}`)
-       fetch(`http://localhost:3000/api/v1/accounts/${data}`)
+       fetch(`https://damp-citadel-47471.herokuapp.com/api/v1/accounts/${data}`)
+       //fetch(`http://localhost:3000/api/v1/accounts/${data}`)
        .then(resp => resp.json())
        .then(accountDetail => dispatch({ type: 'ADD_ACCOUNT_DETAIL', payload: accountDetail }))
        .then(loading => dispatch({ type: 'LOADED_FLAG' }))               
