a2b7be89a2b802693b344e6f7c94c99f3d7a2349
Elliot Blanchard <git@elliotblanchard.com>
2021-04-14 13:35:47 -0400

    Updated fetch

--- src/actions/fetchPools.js
+++ src/actions/fetchPools.js
@@ -1,8 +1,8 @@
 export function fetchPools(time) {
     return (dispatch) => {
        dispatch({ type: 'LOADING_POOLS'})
-       //fetch(`https://damp-citadel-47471.herokuapp.com/api/v1/transactions/${time}`)
-       fetch(`http://localhost:3000/api/v1/pools/${time}`)
+       fetch(`https://damp-citadel-47471.herokuapp.com/api/v1/pools/${time}`)
+       //fetch(`http://localhost:3000/api/v1/pools/${time}`)
           .then(resp => resp.json())
           .then(pools => dispatch({ type: 'ADD_POOLS', payload: pools }))    
     }
